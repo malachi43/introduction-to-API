9cf5fac69adf35b9468eb80abdd68c892e8b6417
Malachi <ukomalachi@gmail.com>
2024-03-27 15:39:55 +0100

    Fix Authentication 'undefined' error

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,7 +2,9 @@ require("dotenv").config()
 const jwt = require("jsonwebtoken")
 
 async function isAuthenticated(req, res, next) {
-    const token = req.headers['authorization'].split(" ")[1]
+    const bearerToken = req.headers['authorization']
+    if (!bearerToken) throw new Error(`please provide an authentication token.`)
+    const token = bearerToken.split(" ")[1]
     const verifiedToken = jwt.verify(token, process.env.JWT_SECRET)
     req.userId = verifiedToken.id
     next()
